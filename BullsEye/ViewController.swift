d5d48c1fa7720121da2f09a8ce1667881da88fe8
Ivan N <spaceshiper1998@gmail.com>
2025-11-03 20:44:26 +0500

    Add gitignore

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -14,6 +14,19 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    override func didReceiveMemoryWarning() {
+        return super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello, world!", message: "This is my first app!", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    
 }
 
