492efebdb3f12e610ffd707cd769a212d685f830
Ivan N <spaceshiper1998@gmail.com>
2025-11-04 09:14:15 +0500

    Adding rounds

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 class ViewController: UIViewController {
 
     var currentValue: Int = 50
+    var targetValue: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+//        currentValue = lroundf(slider.value)
+//        targetValue = 1 + Int(arc4random_uniform(100))
+        startNewRound()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,16 +29,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        //currentValue = 50
+        slider.value = Float(currentValue)
+    }
+    
     @IBAction func showAlert() {
         
-        let message = "The value of the slider is now: \(currentValue)"
-        
+        let message = "The value of the slider is: \(currentValue)" +
+        "\nThe target value is: \(targetValue)"
         
         let alert = UIAlertController(title: "Hello, world!", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
         
+        self.startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
